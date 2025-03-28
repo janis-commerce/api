f40c3624a9337e9e66b615b2a207e2639df6c88e
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2025-03-28 11:46:47 -0300

    Fixed manual type inclusion for Linux envs

--- scripts/build-types.sh
+++ scripts/build-types.sh
@@ -10,6 +10,6 @@ tsc lib/index.js --declaration --allowJs --emitDeclarationOnly --outDir types
 mkdir -p types/types
 cp -r lib/types/* types/types/
 
-sed -i '' -E 's/export \{ (.*) \};/import Request = require(".\/types\/request");\
+sed -i -E 's/export \{ (.*) \};/import Request = require(".\/types\/request");\
 import Response = require(".\/types\/response");\
 export { \1, Request, Response };/' ./types/index.d.ts
