c9b69e6a5676d8ed31eb5ce28114878ef573d56b
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2025-03-28 11:50:02 -0300

    Support for linux and macos when building types

--- scripts/build-types.sh
+++ scripts/build-types.sh
@@ -10,6 +10,13 @@ tsc lib/index.js --declaration --allowJs --emitDeclarationOnly --outDir types
 mkdir -p types/types
 cp -r lib/types/* types/types/
 
+os=$(uname -s)
+if [[ "$os" == "Darwin" ]]; then
+sed -i '' -E 's/export \{ (.*) \};/import Request = require(".\/types\/request");\
+import Response = require(".\/types\/response");\
+export { \1, Request, Response };/' ./types/index.d.ts
+else
 sed -i -E 's/export \{ (.*) \};/import Request = require(".\/types\/request");\
 import Response = require(".\/types\/response");\
 export { \1, Request, Response };/' ./types/index.d.ts
+fi
